0d71309585759880d91228608bce12704e10c951
Joseph Sutton <ebwebdev@gmail.com>
2020-08-15 12:45:00 -0500

    init

---
+++ lib/bashjack.ex
@@ -0,0 +1,5 @@
+defmodule Bashjack.CLI do
+  def main(_argv) do
+    Console.red("Hello world!")
+  end
+end
---
+++ test/bashjack_test.exs
@@ -0,0 +1,8 @@
+defmodule BashjackTest do
+  use ExUnit.Case
+  doctest Bashjack.CLI
+
+  # test "greets the world" do
+  #   assert Bashjack.hello() == :world
+  # end
+end
