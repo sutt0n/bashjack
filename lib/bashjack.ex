0fd2e7d7d001f765a5447b461d31383ed1a916e3
Joseph Sutton <ebwebdev@gmail.com>
2020-08-15 17:26:30 -0500

    counting card (low and high)

--- lib/bashjack.ex
+++ lib/bashjack.ex
@@ -1,7 +1,38 @@
+import Enum
+
 defmodule Bashjack.CLI do
   def main(_argv) do
-    name = ExPrompt.string_required("What is your name?")
+    # name = ExPrompt.string_required("What is your name?")
 
-    ExPrompt.string("Hello, #{name}!")
+    # ExPrompt.string(
+    #   "Hello, #{name}! Welcome to Bashjack, an interactive blackjack game in your bash terminal."
+    # )
+
+    deck = for rank <- '23456789TJQKA', suit <- 'CDHS', do: [suit, rank]
+    hand = deck |> shuffle |> take(2)
+
+    # refactor: determine if hand has an Ace to check high or not
+    [low, high] = [count_hand(hand), count_hand(hand, true)]
+
+    ExPrompt.string("Hand: #{hand}")
+    ExPrompt.string("Hand is: #{to_string(low)} #{to_string(high)}")
+  end
+
+  def card_value(value, high_ace \\ false) do
+    case value do
+      {"Q", "J", "T", "K"} ->
+        10
+
+      {"A"} ->
+        if high_ace == true, do: 11, else: 1
+
+      _ ->
+        ExPrompt.string("Converting #{value}")
+        String.to_integer(value)
+    end
+  end
+
+  def count_hand(hand, high_ace \\ false) do
+    reduce(hand, 0, fn x, acc -> card_value(List.to_string(tl(x)), high_ace) + acc end)
   end
 end
