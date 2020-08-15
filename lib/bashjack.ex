051b852764582a937b34de9d4e7d30996598c4ff
Joseph Sutton <ebwebdev@gmail.com>
2020-08-15 12:48:26 -0500

    added deps

--- lib/bashjack.ex
+++ lib/bashjack.ex
@@ -1,5 +1,7 @@
 defmodule Bashjack.CLI do
   def main(_argv) do
-    Console.red("Hello world!")
+    name = ExPrompt.string_required("What is your name?")
+
+    ExPrompt.string("Hello, #{name}!")
   end
 end
