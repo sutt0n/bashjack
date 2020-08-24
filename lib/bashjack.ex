cf54784b832b613b425bfbc22cead3e1f2b53541
Joseph Sutton <ebwebdev@gmail.com>
2020-08-24 09:32:58 -0500

    added some udp socket stuff to standup a server

--- lib/bashjack.ex
+++ lib/bashjack.ex
@@ -1,35 +1,64 @@
 defmodule Bashjack do
+  @spec main(list) :: no_return()
   def main(_argv) do
-    chip_count = 50
-    IO.puts("\nWelcome to Bashjack. You have #{chip_count} chips.")
-    deck = Deck.create_deck() |> Deck.shuffle_deck()
-    Bashjack.game(deck, chip_count)
+    # chip_count = 50
+    # IO.puts("\nWelcome to Bashjack. You have #{chip_count} chips.")
+    # deck = Deck.create_deck() |> Deck.shuffle_deck()
+    # Bashjack.game(deck, chip_count)
+    IO.puts("\nWelcome to Bashjack.")
+    Bashjack.init_prompt()
   end
 
+  def init_prompt() do
+    init_action =
+      IO.gets(
+        "Press [1] for singleplayer, [2] to connect to multiplayer, and [3] to host multiplayer: "
+      )
+      |> String.trim_trailing()
+      |> String.to_integer()
+
+    case init_action do
+      is_integer when init_action === 1 ->
+        IO.puts("\nWelcome to Bashjack. You have 50 chips.")
+        deck = Deck.create_deck() |> Deck.shuffle_deck()
+        Bashjack.game(deck, 50)
+
+      is_integer when init_action === 2 ->
+        IO.puts("\nConnecting... (TODO)")
+
+      is_integer when init_action === 3 ->
+        Server.launch_server()
+
+      _ ->
+        IO.puts("\nPlease enter a valid option.")
+        Bashjack.init_prompt()
+    end
+  end
+
+  @spec game(list(), number()) :: no_return()
   def game(deck, chip_count) do
-    bet = IO.gets("Place your bet: ") |> String.rstrip() |> String.to_integer()
+    bet = IO.gets("Place your bet: ") |> String.trim_trailing() |> String.to_integer()
 
     case bet do
       is_integer when bet > chip_count ->
         IO.puts("\nYour bet is higher than your chip count.")
-        Bashjack.game(chip_count)
+        Bashjack.game(deck, chip_count)
 
       is_integer when bet > 0 ->
-        # deal players
+        # deal player and house
         [player_hand, deck] = Deck.deal_two(deck)
         [house_hand, deck] = Deck.deal_two(deck)
 
-        IO.puts("\nDealer's hand is:")
-        Hand.print_hand(house_hand)
-
-        IO.puts("\nYour hand is:")
-        Hand.print_hand(player_hand)
-
-      # game loop here
+        Bashjack.action_prompt(deck, player_hand, house_hand, bet, chip_count)
 
       _ ->
         IO.puts("\nPlease enter a number greater than zero.")
         Bashjack.game(deck, chip_count)
     end
+  end
+
+  @spec action_prompt(any, any, any, any, any) :: no_return()
+  def action_prompt(deck, player_hand, house_hand, bet, chip_count) do
+    Hand.print_hand(player_hand)
   end
 end
--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Bashjack.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:socket, "~> 0.3"}
       # {:ex_prompt, "~> 0.1.5"},
       # {:dep_from_hexpm, "~> 0.3.0"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
